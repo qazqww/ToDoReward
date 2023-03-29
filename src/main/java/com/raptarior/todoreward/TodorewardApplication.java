eff0eb224ea8f6ec78b1e4dbfeddbbfc3b7f2131
이진곤 <qazqww@naver.com>
2023-03-29 15:00:32 +0900

    First Commit

---
+++ src/main/java/com/raptarior/todoreward/TodorewardApplication.java
@@ -0,0 +1,13 @@
+package com.raptarior.todoreward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodorewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodorewardApplication.class, args);
+	}
+
+}
